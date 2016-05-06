ca75da834ac48375b15b40650d6f92754073e31a
root <zhangjianfeng@chinac.com>
2016-05-06 10:18:59 +0800

    [bugfix] fix cannot detach root device volume
    delete verification of root device volume in volume detach
    need to change root device volume through detach api
    
    Change-Id: I8d528857de5a09c81488404afce352e3802ec303

--- nova/api/openstack/compute/legacy_v2/contrib/volumes.py
+++ nova/api/openstack/compute/legacy_v2/contrib/volumes.py
@@ -422,9 +422,10 @@ class VolumeAttachmentController(wsgi.Controller):
             for bdm in bdms:
                 if bdm.volume_id != volume_id:
                     continue
-                if bdm.is_root:
-                    msg = _("Can't detach root device volume")
-                    raise exc.HTTPForbidden(explanation=msg)
+                # NOTE:Remove this for detach root volume
+                # if bdm.is_root:
+                #    msg = _("Can't detach root device volume")
+                #    raise exc.HTTPForbidden(explanation=msg)
                 try:
                     self.compute_api.detach_volume(context, instance, volume)
                     found = True
