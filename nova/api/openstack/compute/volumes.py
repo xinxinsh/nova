3ce2d9e82e140fbb0b29c8b1bccc130418e14747
lijunli <lijunli@chinac.com>
2016-10-20 10:54:30 +0800

    Neocu needs to get target of instance when do attaching or detaching.
    Currently VolumeAttachmentController sets skip_policy_check as True which
    will result to the lack of instace target.
    Neocu needs to get id of the instance. So this patch is used to set
    skip_policy_check as False.
    
    Change-Id: I12d92aceff13324051345eafe1a8b3b4ae7a8d5d

--- nova/api/openstack/compute/volumes.py
+++ nova/api/openstack/compute/volumes.py
@@ -319,7 +319,7 @@ class VolumeAttachmentController(wsgi.Controller):
     """
 
     def __init__(self):
-        self.compute_api = compute.API(skip_policy_check=True)
+        self.compute_api = compute.API(skip_policy_check=False)
         self.volume_api = volume.API()
         super(VolumeAttachmentController, self).__init__()
 
