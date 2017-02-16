fcc85fe783824cf295ff106b51ebb35de44415f9
zhangjianfeng <kill51216@163.com>
2017-02-16 21:12:48 +0800

    [bugfixes-backport]Set migration status to 'error' on live-migration  failure
    (A) In resize, confirm-resize and revert-resize operation, migration status
    is marked as 'error' in case of failure for respective operation.
    
    Migration object support is added in live-migration operation, which mark
    migration status to 'failed' if live-migration operation fails in-between.
    
    To make live-migration consistent with resize, confirm-resize and revert-
    resize operation, it needs to mark migration status to 'error' instead of
    'failed' in case of failure.
    
    (B) Apart from consistency, proposed change fixes issue (similar to [1])
    which might occur on live-migration failure as follows:
    If live-migration fails (which sets migration status to 'failed') after
    copying instance files from source to dest node and then user request for
    instance deletion. In that case, delete api will only remove instance
    files from instance.host and not from other host (which could be either
    source or dest node but not instance.host). Since instance is already
    deleted, instance files will remain on other host (not instance.host).
    
    Set migration status to 'error' on live-migration failure, so that
    periodic task _cleanup_incomplete_migrations [2] will remove orphaned
    instance files from compute nodes after instance deletion in above case.
    
    [1] https://bugs.launchpad.net/nova/+bug/1392527
    [2] https://review.openstack.org/#/c/219299/
    
    DocImpact: On live-migration failure, set migration status to 'error'
    instead of 'failed'.
    
    Closes-Bug: 1470420
    (cherry picked from commit d61e15818c1d108275b3286a6665fa3e6540e7e7)
    
    Change-Id: Ie7d706710ae67671967bb63065dc687d832e351f

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -5745,7 +5745,7 @@ class ComputeManager(manager.Manager):
 
                 LOG.exception(_LE('Pre live migration failed at %s'),
                               dest, instance=instance)
-                self._set_migration_status(migration, 'failed')
+                self._set_migration_status(migration, 'error')
                 self._rollback_live_migration(context, instance, dest,
                                               block_migration, migrate_data)
 
@@ -5768,7 +5768,7 @@ class ComputeManager(manager.Manager):
 
             LOG.exception(_LE('Live migration failed.'), instance=instance)
             with excutils.save_and_reraise_exception():
-                self._set_migration_status(migration, 'failed')
+                self._set_migration_status(migration, 'error')
 
         self._notify_about_instance_usage(context, instance,
                                           'live_migration.end')
