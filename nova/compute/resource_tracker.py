eabd4fddee02793f0d14952afb7bd5858f941a2f
zhangjianfeng <zhangjianfeng@chinac.com>
2017-06-14 08:55:17 +0800

    [bugfix]change log level to debug for migrations piring
    For resize/cold-migration it is possible that instance
    already changed host to destination, but no confirm/revert
    has happened yet. In that case resource tracked starts spamming
    errors, because it is impossible to match migration and instance.
    It is safe to lower log level to debug in that case.
    closes-bug: #1555320
    
    Change-Id: I757887e8c2a60cb6ff9c54990baf0a2c290d80a3

--- nova/compute/resource_tracker.py
+++ nova/compute/resource_tracker.py
@@ -31,7 +31,7 @@ from nova.compute import resources as ext_resources
 from nova.compute import task_states
 from nova.compute import vm_states
 from nova import exception
-from nova.i18n import _, _LE, _LI, _LW
+from nova.i18n import _, _LI, _LW
 from nova import objects
 from nova.objects import base as obj_base
 from nova.objects import migration as migration_obj
@@ -550,8 +550,12 @@ class ResourceTracker(object):
                 # let the code either fail or lazy-load the instance later
                 # which is what happened before we added this optimization.
                 # This _should_ not be possible, of course.
-                LOG.error(_LE('Migration for instance %(uuid)s refers to '
-                              'another host\'s instance!'),
+                # NOTE(tdurakov) this situation is possible for resize/cold
+                # migration when migration is finished but haven't yet
+                # confirmed/reverted in that case instance already changed host
+                # to destination and no matching happens
+                LOG.debug('Migration for instance %(uuid)s refers to '
+                              'another host\'s instance!',
                           {'uuid': migration.instance_uuid})
 
     @utils.synchronized(COMPUTE_RESOURCE_SEMAPHORE)
